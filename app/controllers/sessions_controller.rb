1a3ba1ea6a7c8cbce7386c3463734a7f411d2519
KYLE LIPTON <lipton.kyle@gmail.com>
2017-11-29 19:13:13 -0500

    photogur with authentication

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to pictures_url, notice: "Logged in!"
+    else
+      render "new"
+    end
+  end
+
+  def new
+
+  end
+
+  def 
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to pictures_url, notice: "Logged out!"
+  end
+
+end
